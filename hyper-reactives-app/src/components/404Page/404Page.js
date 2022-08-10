723c639b58ec2ecbfb6c992bd7b4c51807c5f9ed
PramodSony <sony.p@northeastern.edu>
2022-08-10 04:09:56 -0400

    added 404 page

---
+++ hyper-reactives-app/src/components/404Page/404Page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./404Page.scss"
+
+
+const PageNotFound = () =>{
+
+    return (
+        
+
+        
+        <div class="errorbody">
+        <div class="container">
+            <img class="ops" src="/404.svg" />
+            <br />
+            <h3 >Page not found</h3>
+            
+        
+        </div>
+        </div>
+        
+    )
+
+}
+
+
+export default PageNotFound;
